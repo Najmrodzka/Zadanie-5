b57427a40abc0272e4e55f47556f3c425b803b28
Najmrodzka <101128573+Najmrodzka@users.noreply.github.com>
2022-06-04 17:27:51 +0200

    Add files via upload

---
+++ Zadanie5/Zadanie5.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+}
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+
+
